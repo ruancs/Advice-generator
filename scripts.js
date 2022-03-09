c6f92d4891826e9e89037bbc0c492759b466a6c5
ruancs <ruan.cs@hotmail.com>
2022-03-09 17:24:16 -0300

    adicionado fetch e codigo de API para quotes

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+const btn = document.querySelector('.btn-random');
+const id_tag = document.querySelector('.advice-id');
+const advice_tag = document.querySelector('.advice');
+
+function onBtnClick(){
+    console.log('click');
+    fetch('https://api.adviceslip.com/advice', {cache: 'no-store'})
+        .then(resp => resp.json())
+        .then(jsonData => {
+            id_tag.innerText = 'ADVICE #' + jsonData['slip']['id'];
+            advice_tag.innerText = '"' + jsonData['slip']['advice'] + '"';
+        });
+    
+}
+
+btn.addEventListener('click', onBtnClick);
+
+onBtnClick();
+
+
+
